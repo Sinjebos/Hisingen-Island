0fc4ecf7df378230b20f1fad089afd31bc5a0de8
nienik <niklas.nielsen@stralfors.se>
2024-05-10 14:38:44 +0200

    updated restart_on_timer

--- restart_on_timer.sh
+++ restart_on_timer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOG_FILE="/home/niklas/mc/spigot/logs/latest.log"
+LOG_FILE="/home/niklas/Documents/Enimatica/logs/latest.log"
 
 curl -H "Content-Type: application/json" -X POST -d '{"content":"Minecraft server will be restarting in 5 minutes...\nNext restart will be in 12 hours..."}'
 
@@ -12,20 +12,21 @@ sleep 20
 
 sudo systemctl start mc.service
 
-sleep 200
+exit 0
 
-# Check if server started up properly else restart againi
+# sleep 200
+# # Check if server started up properly else restart againi
 
-while true; do
-    echo "Checking server status..."
-    # Check if the server started up properly
-    if grep -q "Dedicated server took .* seconds to load" "$LOG_FILE"; then
-        echo "Server started successfully."
-      	exit 0
-    else
-        echo "Server did not start properly. Restarting..."
-        # Restart the server
-        sudo systemctl restart mc.service
-        sleep 200  # Sleep for 3 minutes before checking again
-    fi
-done
+# while true; do
+#     echo "Checking server status..."
+#     # Check if the server started up properly
+#     if grep -q "Dedicated server took .* seconds to load" "$LOG_FILE"; then
+#         echo "Server started successfully."
+#       	exit 0
+#     else
+#         echo "Server did not start properly. Restarting..."
+#         # Restart the server
+#         sudo systemctl restart mc.service
+#         sleep 200  # Sleep for 3 minutes before checking again
+#     fi
+# done
